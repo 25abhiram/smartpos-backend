d6a96263f0062873e151e6f9d8853ebe346dd204
25abhiram <ravikumarabhiram7@gmail.com>
2026-07-24 15:22:31 +0530

    feat: implement get, update, and delete endpoints for branch management

--- src/main/java/com/smartpos/backend/controller/BranchController.java
+++ src/main/java/com/smartpos/backend/controller/BranchController.java
@@ -5,10 +5,9 @@ import com.smartpos.backend.service.BranchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/branch")
@@ -20,6 +19,30 @@ public class BranchController {
     public ResponseEntity<Branch> createBranch(@RequestBody Branch branch){
         Branch createdBranch=branchService.createBranch(branch);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdBranch);
+    }
+
+    @GetMapping("/allbranches")
+    public ResponseEntity<List<Branch>> getAllBranches(){
+        List<Branch> branches=branchService.getAllBranches();
+        return ResponseEntity.ok(branches);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Branch> getBranchById(@PathVariable Long id){
+        Branch branch=branchService.getBranchById(id);
+        return ResponseEntity.ok(branch);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Branch> updateBranch(@PathVariable Long id,@RequestBody Branch branch){
+        Branch updatedBranch=branchService.updateBranch(id,branch);
+        return ResponseEntity.ok(updatedBranch);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBranchById(@PathVariable Long id){
+        branchService.deleteBranchById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/smartpos/backend/service/BranchService.java
+++ src/main/java/com/smartpos/backend/service/BranchService.java
@@ -1,9 +1,12 @@
 package com.smartpos.backend.service;
 
 import com.smartpos.backend.entity.Branch;
+import com.smartpos.backend.exceptions.ResourceNotFoundException;
 import com.smartpos.backend.repository.BranchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BranchService {
@@ -12,4 +15,26 @@ public class BranchService {
     public Branch createBranch(Branch branch){
         return branchRepository.save(branch);
     }
+
+    public List<Branch> getAllBranches(){
+        List<Branch> branches=branchRepository.findAll();
+        return branches;
+    }
+
+    public Branch getBranchById(Long id){
+        return branchRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Branch not found with id "+id));
+    }
+
+    public Branch updateBranch(Long id,Branch branch){
+        Branch branchData=branchRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Branch not found with id "+id));
+        branchData.setName(branch.getName());
+        branchData.setAddress(branch.getAddress());
+        return branchRepository.save(branchData);
+    }
+
+    public void deleteBranchById(Long id){
+        Branch branchData=branchRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Branch not found with id "+id));
+        branchRepository.delete(branchData);
+    }
+
 }
